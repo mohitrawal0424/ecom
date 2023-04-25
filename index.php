83fc41f7a13b4f83f36861b17fa0fc32855c9a31
mohitrawal0424 <112812307+mohitrawal0424@users.noreply.github.com>
2023-04-25 23:14:32 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
+    <link rel="stylesheet" href="css/navbar.css">
+    <!-- imported google font  -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Staatliches&display=swap" rel="stylesheet">
+<style>
+*{
+    font-family: 'Staatliches', cursive;
+}
+</style>
+</head>
+<body>
+    
+    <!-- navbar-->
+<section>
+<!-- HTML code for navbar -->
+<nav class="navbar">
+  <div class="logo">
+    <a href="#"><img src="images/logo/logo.png" alt="Logo"></a>
+  </div>
+  <div class="menu">
+    <ul class="menu-links">
+      <li><a href="#">Home</a></li>
+      <li><a href="#">Products</a></li>
+      <li><a href="#">About Us</a></li>
+    </ul>
+  </div>
+  <div class="cart">
+    <a href="#">Cart</a>
+  </div>
+</nav>
+
+</section>
+<script src="https://kit.fontawesome.com/0ef3c59d0f.js" crossorigin="anonymous"></script>
+</body>
+</html>
